ba6caf92e093d8cbb942aa3f5b5949beb5cad6df
Catherine Lin <catherine.lin.93@gmail.com>
2018-07-24 23:51:49 -0700

    album and nav finished

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  $('.nav-link').click(function() {
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top
+      },
+      800
+    );
+
+    return false;
+  });
+
   var lastScrollTop = 0;
   $(window).scroll(function(event) {
     var st = $(this).scrollTop();
@@ -10,5 +21,18 @@ $(document).ready(function() {
       $('.navigation').removeClass('header-sticky');
     }
     lastScrollTop = st;
+  });
+
+  $('#album-modal').iziModal({ width: 1000 });
+
+  $('.album-trigger').on('click', function(event) {
+    console.log(event.target.id);
+    event.preventDefault();
+    $('#album-modal').html(
+      `<img src="./img/album-${event.target.id}.jpg" alt="">`
+    );
+    $('#album-modal').iziModal('setZindex', 99999);
+    $('#album-modal').iziModal('open', { zindex: 99999 });
+    $('#album-modal').iziModal('open');
   });
 });
