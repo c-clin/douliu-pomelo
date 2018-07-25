e87c5c4e5fa762d0d43fa9c30ac4c4591eea1fd0
Catherine Lin <catherine.lin.93@gmail.com>
2018-07-25 01:08:26 -0700

    added footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // smooth scrolling
   $('.nav-link').click(function() {
     $('html, body').animate(
       {
@@ -10,6 +11,7 @@ $(document).ready(function() {
     return false;
   });
 
+  // adjust the nav style base on scroll
   var lastScrollTop = 0;
   $(window).scroll(function(event) {
     var st = $(this).scrollTop();
@@ -23,16 +25,22 @@ $(document).ready(function() {
     lastScrollTop = st;
   });
 
+  // open modal to view images
   $('#album-modal').iziModal({ width: 1000 });
 
   $('.album-trigger').on('click', function(event) {
     console.log(event.target.id);
     event.preventDefault();
     $('#album-modal').html(
-      `<img src="./img/album-${event.target.id}.jpg" alt="">`
+      `<img src="./img/album-${event.target.id}.jpg" width="940">`
     );
     $('#album-modal').iziModal('setZindex', 99999);
     $('#album-modal').iziModal('open', { zindex: 99999 });
     $('#album-modal').iziModal('open');
   });
+
+  if ($(window).width() < 1200) {
+    $('.album__list--photo').removeClass('album-trigger');
+    console.log('removed');
+  }
 });
